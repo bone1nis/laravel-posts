2d75c61c788209e89563ff8dd4f0b0630ba91264
bone1nis <bone1nis@gmail.com>
2025-04-06 20:36:54 +0300

    refactor: Move update and store logic to service layer

--- app/Http/Controllers/Post/DestroyController.php
+++ app/Http/Controllers/Post/DestroyController.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Controllers\Post;
 
-use App\Http\Controllers\Controller;
-use App\Models\Category;
 use App\Models\Post;
-use App\Models\Tag;
 
-class DestroyController extends Controller
+class DestroyController extends BaseController
 {
 
     public function __invoke(Post $post)
